d11f27dde058ce000fd26fb919fefe4a8a9a09d3
Mojack <14106852+momolder@users.noreply.github.com>
2023-12-08 12:06:58 +0100

    add copy functionality to markdown code-blocks

--- src/lib/components/chat/components/code-block.svelte
+++ src/lib/components/chat/components/code-block.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+  import clipboard from '$lib/assets/clipboard.svg';
   import { isNullOrWhitespace } from '$lib/helper';
+  import { ToastErrors } from '$lib/services/error-handler';
   import themingService from '$lib/services/theming-service';
   import { getHighlighter, setCDN, type Lang, type Highlighter, BUNDLED_LANGUAGES } from 'shiki';
   export let lang: string;
@@ -29,11 +31,25 @@
     }
     return highlighter;
   }
+
+  async function copyToClipboard() {
+    await navigator.clipboard.writeText(text).catch(ToastErrors);
+  }
 </script>
 
-<div class="bg-light-highlight dark:bg-dark-base rounded-lg flex flex-col my-4 border border-light-highlight dark:border-dark-base">
-  <div class="text-sm p-2">{lang}</div>
-{#await highlight().then( (c) => c?.codeToHtml( text, { lang: isNullOrWhitespace(lang) ? 'bash' : lang, theme: themingService.isDark() ? darkTheme : lightTheme } ) ) then code}
-          <div class="rounded-b-lg overflow-x-auto bg-light-codeblock dark:bg-dark-codeblock p-2"><code>{@html code}</code></div>
-{/await}
+<div
+  class="bg-light-highlight dark:bg-dark-base rounded-lg flex flex-col my-4 border border-light-highlight dark:border-dark-base">
+  <div class="flex flex-row justify-between px-4 py-2">
+    <div class="text-sm">{lang}</div>
+    <button
+      class="btn h-4 flex flex-row items-center gap-1 text-sm"
+      type="button"
+      on:click={async (e) => await copyToClipboard()}
+      ><img class="ico h-4 w-4" src={clipboard} alt="logo" />Copy code</button>
+  </div>
+  {#await highlight().then( (c) => c?.codeToHtml( text, { lang: isNullOrWhitespace(lang) ? 'bash' : lang, theme: themingService.isDark() ? darkTheme : lightTheme } ) ) then code}
+    <div class="rounded-b-lg overflow-x-auto bg-light-codeblock dark:bg-dark-codeblock p-2">
+      <code>{@html code}</code>
+    </div>
+  {/await}
 </div>
