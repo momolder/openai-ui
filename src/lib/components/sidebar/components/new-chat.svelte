ba818ba09a7c7c20970614db2d2f6800b0ce8b41
Didem Evran <Didem.Evran@adesso.de>
2023-12-19 16:49:41 +0100

    fix translation for title

--- src/lib/components/sidebar/components/new-chat.svelte
+++ src/lib/components/sidebar/components/new-chat.svelte
@@ -3,6 +3,7 @@
   import clear from '$lib/assets/clear.svg';
 
   export let showLabel = false;
+  export let label: string;
 
   function clearChat() {
     conversationService.new();
@@ -11,5 +12,5 @@
 
 <button name="newChat" data-testid="inputClearButton" class="sidebar-btn p-2" type="button" on:click={clearChat}>
   <img class="ico h-5 w-5" src={clear} alt="clear chat" />
-  <p class="{showLabel ? 'visible' : 'hidden'} text-center justify-self-center">New chat</p>
+  <p class="{showLabel ? 'visible' : 'hidden'} text-center justify-self-center">{label}</p>
 </button>
