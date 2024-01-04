e1cfc252a55cd7e41e9c657a5fd50abedd1f0373
Mojack <14106852+momolder@users.noreply.github.com>
2024-01-04 14:34:31 +0100

    use database service singleton

--- src/routes/history/+server.ts
+++ src/routes/history/+server.ts
@@ -1,21 +1,18 @@
 import { json, type RequestEvent } from '@sveltejs/kit';
 import type { Conversation } from '$lib/models/Contracts.js';
-import DatabaseService from '$lib/services/database-service';
+import databaseService from '$lib/services/database-service';
 
 export async function POST({ request }: RequestEvent): Promise<Response> {
-  const databaseService = await new DatabaseService().init();
   const conversation = (await request.json()) as Conversation;
   return json(await databaseService.createHistory(conversation));
 }
 
 export async function PUT({ request }: RequestEvent): Promise<Response> {
-  const databaseService = await new DatabaseService().init();
   const conversation = (await request.json()) as Conversation;
   return json(await databaseService.updateHistory(conversation));
 }
 
 export async function DELETE({ request }: RequestEvent): Promise<Response> {
-  const databaseService = await new DatabaseService().init();
   const conversation = (await request.json()) as Conversation;
   await databaseService.deleteHistory(conversation);
   return new Response(undefined, { status: 204 });
