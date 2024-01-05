b66c411dc79028e1e1cdf9981b864b38351d781d
Mojack <14106852+momolder@users.noreply.github.com>
2024-01-05 08:07:48 +0100

    revert use database service singleton
    - change causes build failure in git action

--- src/routes/history/user/[userId]/+server.ts
+++ src/routes/history/user/[userId]/+server.ts
@@ -1,11 +1,13 @@
 import { json } from '@sveltejs/kit';
-import databaseService from '$lib/services/database-service';
+import DatabaseService from '$lib/services/database-service';
 import type { RequestEvent } from './$types.js';
 
 export async function GET({ params }: RequestEvent): Promise<Response> {
+  const databaseService = await new DatabaseService().init();
   return json(await databaseService.getHistory(params.userId));
 }
 
-export function DELETE({ params }: RequestEvent): Response {
+export async function DELETE({ params }: RequestEvent): Promise<Response> {
+  const databaseService = await new DatabaseService().init();
   return json(databaseService.deleteUserHistory(params.userId));
 }
