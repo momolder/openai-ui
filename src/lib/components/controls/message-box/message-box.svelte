756ca5ca1ea001308f8a30760202b37df38af717
Mojack <14106852+momolder@users.noreply.github.com>
2024-02-16 14:20:10 +0100

    message box support for text input

--- src/lib/components/controls/message-box/message-box.svelte
+++ src/lib/components/controls/message-box/message-box.svelte
@@ -16,10 +16,10 @@
 <dialog bind:this={dialog} class="overflow-hidden">
   <!-- svelte-ignore a11y-no-static-element-interactions -->
   <div
-    class="cmp flex flex-col bg-light-base dark:bg-dark-base border border-light-highlight dark:border-dark-highlight  text-light-text dark:text-dark-text">
+    class="cmp flex flex-col bg-light-base dark:bg-dark-base border border-light-highlight dark:border-dark-highlight text-light-text dark:text-dark-text">
     <div class="flex flex-row justify-between bg-light-overlay dark:bg-dark-overlay">
       <div class="cmp px-2 py-1 flex flex-row items-center justify-start">
-        <img class="ico h-8 w-8" src={$MessageBoxStore.icon} alt="messagebox icon" />
+        <img class="ico h-8 w-8 dark:invert-0" src={$MessageBoxStore.icon} alt="messagebox icon" />
         <h4 class="pl-4">{$MessageBoxStore.title}</h4>
       </div>
     </div>
@@ -27,11 +27,15 @@
     <p class="cmp p-2 overflow-auto">
       {$MessageBoxStore.message}
     </p>
-    
+    {#if $MessageBoxStore.input && $MessageBoxStore.input.type === 'text'}
+    <input class="outline-none m-2 border-b-2 border-light-highlight dark:border-dark-highlight" type='text' placeholder={$MessageBoxStore.input.placeholder} bind:value={$MessageBoxStore.input.value} />
+    {/if}
     <div class="flex flex-row justify-between">
       <!-- svelte-ignore a11y-autofocus -->
-      <button class="btn rounded-none px-2" autofocus on:click={() => close(true)}>{$MessageBoxStore.okLabel}</button>
-      <button class="btn rounded-none px-2" on:click={() => close(false)}>{$MessageBoxStore.cancelLabel}</button>
+      <button class="btn rounded-none px-2" autofocus on:click={() => close(true)}
+        >{$MessageBoxStore.okLabel}</button>
+      <button class="btn rounded-none px-2" on:click={() => close(false)}
+        >{$MessageBoxStore.cancelLabel}</button>
     </div>
   </div>
 </dialog>
--- src/lib/components/controls/message-box/message-box.ts
+++ src/lib/components/controls/message-box/message-box.ts
@@ -11,6 +11,8 @@ export const MessageBoxStore = writable<{
   ok: boolean;
   okLabel: string;
   cancelLabel: string;
+  slot?: string;
+  input?: input;
 }>({
   showMessage: false,
   icon: '',
@@ -21,7 +23,11 @@ export const MessageBoxStore = writable<{
   cancelLabel: 'Cancel'
 });
 
-export async function showMessageBox(type: 'info'|'warning'|'error',message: string, title?: string, okLabel?: string, cancelLabel?: string, customIcon?: string): Promise<boolean> {
+export interface input {
+  type: 'text', placeholder: string, value?: string
+}
+
+export async function showMessageBox(type: 'info'|'warning'|'error',message: string, title?: string, okLabel?: string, cancelLabel?: string, customIcon?: string, input?: input): Promise<boolean> {
   const icon = type === 'info' ? info : type === 'warning' ? warning : error;
   MessageBoxStore.set({
     showMessage: true,
@@ -30,7 +36,8 @@ export async function showMessageBox(type: 'info'|'warning'|'error',message: str
     message: message,
     ok: false,
     okLabel: okLabel ?? 'Ok',
-    cancelLabel: cancelLabel ?? 'Cancel'
+    cancelLabel: cancelLabel ?? 'Cancel',
+    input: input
   });
 
   let dialogOpen = false;
