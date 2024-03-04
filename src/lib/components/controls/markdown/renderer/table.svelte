a9e2ad0e4458d31ba58d294bdea653b5fa9a907c
Mojack <14106852+momolder@users.noreply.github.com>
2024-03-04 16:18:04 +0000

    fix large tables

--- src/lib/components/controls/markdown/renderer/table.svelte
+++ src/lib/components/controls/markdown/renderer/table.svelte
@@ -1,1 +1,3 @@
-<table class="min-w-full divide-y divide-light-highlight dark:divide-dark-highlight"><slot /></table>
+<div class="w-full overflow-auto">
+  <table class="min-w-full divide-y divide-light-highlight dark:divide-dark-highlight"><slot /></table>
+</div>
