d5a2f7284fe7131e49a2b959c5920b6821da4ad1
Mojack <14106852+momolder@users.noreply.github.com>
2024-03-05 16:22:48 +0000

    add server-side debugging with vavite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { nodeLoaderPlugin } from "@vavite/node-loader/plugin";
 
 export default defineConfig(() => {
   return {
-    plugins: [sveltekit()],
+    plugins: [sveltekit(), nodeLoaderPlugin()],
     server: {
       port: 3000
     }
