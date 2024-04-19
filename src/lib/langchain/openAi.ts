b19a22dc8794f820f50a5941fd9b00caea8eeed2
Mojack <14106852+momolder@users.noreply.github.com>
2024-04-19 11:08:23 +0000

    demo

--- src/lib/langchain/openAi.ts
+++ src/lib/langchain/openAi.ts
@@ -11,10 +11,23 @@ import type { Document } from '@langchain/core/documents';
 import { ConsoleCallbackHandler } from '@langchain/core/tracers/console';
 import { OpenAIEmbeddings } from '@langchain/openai';
 import { RunnableSequence } from '@langchain/core/runnables';
-import { type ChainInput, chatModeTemplates, rephraseTemplate, promptTemplate, citationSchema } from './templates';
+import {
+  type ChainInput,
+  chatModeTemplates,
+  rephraseTemplate,
+  promptTemplate,
+  citationSchema
+} from './templates';
 import { StructuredOutputParser } from 'langchain/output_parsers';
+import { CheerioWebBaseLoader } from 'langchain/document_loaders/web/cheerio';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { MemoryVectorStore } from 'langchain/vectorstores/memory';
+import { WebPDFLoader } from 'langchain/document_loaders/web/pdf';
+import { TextLoader } from 'langchain/document_loaders/fs/text';
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
 
 export async function response(conversation: Conversation, chatMode: ChatMode, deployment: string) {
+  // await indexDocuments(); return;
   const mappedMessages = mapMessages(conversation); // map messages to AIMessage and HumanMessage
   mappedMessages.pop(); // remove the placeholder for the response
   const question = mappedMessages.pop(); // remove the question
@@ -27,22 +40,17 @@ export async function response(conversation: Conversation, chatMode: ChatMode, d
     context: provideContext(llm),
     question: (input: ChainInput) => input.question.content.toString(),
     chatHistory: (input: ChainInput) => input.chat_history,
-    format_instructions: (input: ChainInput) => input.format_instructions,
+    format_instructions: (input: ChainInput) => input.format_instructions
   };
 
-  const runnableChain = RunnableSequence.from([
-    runnable,
-    promptTemplate,
-    llm,
-    new StringOutputParser()
-  ]);
+  const runnableChain = RunnableSequence.from([runnable, promptTemplate, llm, new StringOutputParser()]);
 
   if (question) {
     const stream = await runnableChain.stream({
       question: question,
       chat_history: mappedMessages,
-      format_instructions: parser.getFormatInstructions(),
-  });
+      format_instructions: parser.getFormatInstructions()
+    });
 
     return new Response(stream, {
       headers: {
@@ -55,8 +63,13 @@ export async function response(conversation: Conversation, chatMode: ChatMode, d
 function provideContext(llm: AzureChatOpenAI) {
   return RunnableSequence.from([
     (input: ChainInput) => rephraseQuestion(input, llm),
-    getVectorStore().asRetriever(),
-    (docs: Document[]) => docs.map((doc, i) => {console.log(doc.metadata); return `{ id: '${i}', source: '${doc.metadata.source}', content: ${doc.pageContent}}`;}).join(',\n')
+    getRetriever(),
+    (docs: Document[]) =>
+      docs
+        .map((doc, i) => {
+          return `{ id: '${i}', source: '${doc.metadata.source}', content: ${doc.pageContent}}`;
+        })
+        .join(',\n')
   ]);
 }
 
@@ -114,6 +127,33 @@ function getEmbbedding() {
   });
 }
 
+function getRetriever() {
+  return getVectorStore().asRetriever();
+}
+
+async function getInMemoryRetriever() {
+  const glossarUrl = 'https://www.gdv.de/resource/blob/6320/b4588b93ac8c40299a4bde55cf81c46b/wichtige-begriffe-fuer-die-lebensversicherung-im-ueberblick-data.pdf';
+  const allgemeineBedingungenUrl = 'https://www.gdv.de/resource/blob/6326/43afd7c4e9d11e5275109fa05bc1c4d9/01-allgemeine-bedingungen-fur-die-berufsunfahigkeits-versicherung-0-pdf-data.pdf';
+  const blob = await fetch(allgemeineBedingungenUrl).then(r => r.blob());
+  const loader = new WebPDFLoader(blob);
+
+  // const loader = new CheerioWebBaseLoader(
+  //   "https://lilianweng.github.io/posts/2023-06-23-agent/"
+  // );
+
+  const docs = await loader.load();
+
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 200
+  });
+  const splits = await textSplitter.splitDocuments(docs);
+  splits.forEach(s => s.metadata.source = allgemeineBedingungenUrl);
+  const vectorStore = await MemoryVectorStore.fromDocuments(splits, getEmbbedding());
+
+  return vectorStore.asRetriever();
+}
+
 function getVectorStore() {
   return new AzureAISearchVectorStore(getEmbbedding(), {
     search: {
@@ -123,4 +163,16 @@ function getVectorStore() {
     indexName: env.AiSearch_IndexName,
     key: env.AiSearch_Key
   });
+}
+
+async function indexDocuments() {
+  const loader = new PDFLoader('./static/01-allgemeine-bedingungen-fur-die-berufsunfahigkeits-versicherung.pdf');
+  const rawDocuments = await loader.load();
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 0
+  });
+  const documents = await splitter.splitDocuments(rawDocuments);
+  console.log(documents);
+  await getVectorStore().addDocuments(documents);
 }
